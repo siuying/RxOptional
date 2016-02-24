7c20459b3e07e2f612ea43d85d23bd373152e986
Francis Chong <francis@ignition.hk>
2016-02-25 00:50:38 +0800

    Use Carthage

--- Example/Pods/Target Support Files/Pods-RxOptional Examples/Pods-RxOptional Examples-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_RxOptional_ExamplesVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RxOptional_ExamplesVersionString[];
-
---
+++ RxOptional/RxOptional.h
@@ -0,0 +1,19 @@
+//
+//  RxOptional.h
+//  RxOptional
+//
+//  Created by Chan Fai Chong on 25/2/2016.
+//  Copyright © 2016 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxOptional.
+FOUNDATION_EXPORT double RxOptionalVersionNumber;
+
+//! Project version string for RxOptional.
+FOUNDATION_EXPORT const unsigned char RxOptionalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxOptional/PublicHeader.h>
+
+
